50d51a84f0eda4e672c30b1a989e0fa82fbb94ce
Steve Tu <stevetu11@yahoo.com>
2022-07-13 23:11:38 -0700

    Save the Konami ID as well as saving the JSON file for frontend to use

--- android/app/src/main/assets/data/main.py
+++ android/app/src/main/assets/data/main.py
@@ -1,10 +1,12 @@
 import json
 import requests
+import os
 
 class CardScraper:
     def start(self):
         page_number = 1
         card_list = []
+
         while True:
             # Iterate through each page until it returns an empty json array.
             response = requests.get(f"https://www.masterduelmeta.com/api/v1/cards?page={page_number}&limit=3000")
@@ -14,13 +16,17 @@ class CardScraper:
                 print(f"{len(card_list)} Cards")
                 with open("cards.json", "w", encoding = "utf-8") as file:
                     json.dump(card_list, file, indent=4)
+
+                with open(os.path.dirname(__file__) + "../../../../../../src/data/cards.json", "w", encoding = "utf-8") as file:
+                    json.dump(card_list, file, indent=4)
+
                 return
 
             for card in data:
                 # Filter out cards that are not in Master Duel yet and then append the rest to the list.
                 try:
                     card["rarity"]
-                    card_list.append({"name": card["name"], "rarity": card["rarity"]})
+                    card_list.append({"name": card["name"], "rarity": card["rarity"], "konamiID": card["konamiID"] })
                 except KeyError:
                     pass
 
